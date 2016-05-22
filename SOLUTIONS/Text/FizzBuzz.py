08694ed38e577648de7cd14e232a4d4aa9fc4363
JudgeVector <judgevector@gmail.com>
2016-05-22 03:56:02 -0400

    First commit

---
+++ SOLUTIONS/Text/FizzBuzz.py
@@ -0,0 +1,19 @@
+""" 
+Fizz Buzz - Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. 
+For numbers which are multiples of both three and five print “FizzBuzz”.
+"""
+
+def fizzbuzz(n):
+	if n % 3 == 0 and n % 5 == 0:
+		return 'FizzBuzz'
+	elif n % 3 == 0:
+		return 'Fizz'
+	elif n % 5 == 0:
+		return 'Buzz'
+	else:
+		return n
+
+for i in range(1,101):
+	print(fizzbuzz(i))
+	
+input("Press enter to exit")
